e3470c05c6cb45d9b7caf96a342ec6c476794eb8
zulhamsy <zulham.sy@gmail.com>
2022-07-18 12:27:16 +0700

    add Login Page with responsive to mobile

---
+++ src/pages/Login.jsx
@@ -0,0 +1,24 @@
+import { Container, Row, Col, Image } from "react-bootstrap";
+import ProfilePicture from "../assets/ProfilePicture.png";
+import LoginForm from "../components/LoginForm";
+
+const Login = () => {
+  return (
+    <Container className="bg-dark">
+      <Row className="g-0 min-vh-100 align-items-center">
+        {/* Image Placeholder */}
+        <Col className="d-none d-md-block">
+          <Image src={ProfilePicture} fluid />
+        </Col>
+        {/* Login Form */}
+        <Col className="d-flex justify-content-center">
+          <div className="bg-dark w-75">
+            <LoginForm />
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default Login;
