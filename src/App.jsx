123c0ae5e527802a13dc668024e7d7d93f4e4c6a
Aryan Jaya <aryanjaya88@gmail.com>
2022-07-19 17:53:41 +0800

    add footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Footer from "./components/Footer";
 import NavbarMenu from "./components/NavbarMenu";
 import "./App.css";
 
@@ -5,6 +6,7 @@ function App() {
   return (
     <div className="App">
       <NavbarMenu />
+      <Footer />
     </div>
   );
 }
