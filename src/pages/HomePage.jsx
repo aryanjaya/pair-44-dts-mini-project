dd36c27a8155cfd827f31a6faa08f2899a894ca9
Aryan Jaya <aryanjaya88@gmail.com>
2022-07-19 23:40:14 +0800

    add base movie list layout

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,14 @@
+import MovieList from "../components/movie/MovieList";
+
 const HomePage = () => (
-  <>
-    <h1>Homepage</h1>
-  </>
+  <section className="section">
+    <hgroup className="section-title">
+      <h1>What to Watch</h1>
+    </hgroup>
+    <section className="section-body">
+      <MovieList />
+    </section>
+  </section>
 );
 
 export default HomePage;
