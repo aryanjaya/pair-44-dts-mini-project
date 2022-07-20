b5264b707f7c164aaa0ab7f6b941bfe74f948ade
Aryan Jaya <aryanjaya88@gmail.com>
2022-07-20 15:36:32 +0800

    add list movie component

--- src/components/movie/MovieListCard.jsx
+++ src/components/movie/MovieListCard.jsx
@@ -1,22 +1,32 @@
 import { faStar } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Card from "react-bootstrap/Card";
+import TmdbApi from "../../services/tmdb";
 
-const MovieListCard = () => {
+const MovieListCard = ({ movie }) => {
+  const getYear = (date) => {
+    date = new Date(date);
+
+    return date.getFullYear();
+  };
+
+  const getImage = (path) => TmdbApi.getImage(path, 'w342');
+
   return (
     <Card bg="darker" text="white">
       <Card.Img
         variant="top"
-        src="https://m.media-amazon.com/images/M/MV5BNWM0ZGJlMzMtZmYwMi00NzI3LTgzMzMtNjMzNjliNDRmZmFlXkEyXkFqcGdeQXVyMTM1MTE1NDMx._V1_QL75_UX280_CR0,0,280,414_.jpg"
+        src={getImage(movie.poster_path)}
       ></Card.Img>
       <Card.Body>
-        <Card.Title>Doctor Strange</Card.Title>
+        <Card.Title>{movie.title}</Card.Title>
         <Card.Subtitle>
           <div className="d-flex justify-content-between">
             <span>
-              <FontAwesomeIcon icon={faStar} className="rating-star" /> 5.6
+              <FontAwesomeIcon icon={faStar} className="rating-star" />{" "}
+              {movie.vote_average}
             </span>
-            2022
+            {getYear(movie.release_date)}
           </div>
         </Card.Subtitle>
       </Card.Body>
---
+++ src/services/tmdb/MovieApi.js
@@ -0,0 +1,34 @@
+import { AxiosError } from "axios";
+import client from "./client";
+
+const MovieApi = {
+  async getNowPlaying(options = {}){
+    let data = null;
+    let errors = null;
+    let statusCode = null;
+
+    try {
+      const response = await client.get("/movie/now_playing", {
+        params: options,
+      });
+
+      data = response.data;
+      statusCode = response.status;
+    } catch (error) {
+      if (error instanceof AxiosError) {
+        errors = error.response.data;
+        statusCode = error.response.status;
+      } else {
+        errors = error;
+      }
+    }
+
+    return {
+      data,
+      errors,
+      statusCode,
+    };
+  },
+};
+
+export default MovieApi;
